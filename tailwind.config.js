96ee060d2c1fcb5153d9d919d3a17504879ec664
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-03-03 19:02:33 +0800

    tailwindcss 添加‘过滤未使用样式’配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './pages/**/*.js',
+    './pages/**/*.tsx',
+    './components/**/*.js',
+    './components/**/*.tsx'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
