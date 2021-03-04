421807361d74eaaf0c1a766e61fa7d76c2e33af8
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-03-04 11:42:32 +0800

    尝试修复favicon不显示问题

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@ import "tailwindcss/tailwind.css"
 import Head from 'next/head'
 import PageHeader from '../components/page-header'
 import Donators from '../components/donators'
+import favicon from '../public/favicon.ico'
 function MyApp({ Component, pageProps }) {
   return <div className="App max-w-3xl mx-auto pb-6">
     <Head>
       <title>文章收藏 - next</title>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href={favicon} />
     </Head>
     <PageHeader/>
     <Component {...pageProps} />
