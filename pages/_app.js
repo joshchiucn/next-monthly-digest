7eb480b4c1f00e80ec601cb12602cb43081e3c63
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-03-04 11:47:45 +0800

    favicon路径

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,12 @@ import "tailwindcss/tailwind.css"
 import Head from 'next/head'
 import PageHeader from '../components/page-header'
 import Donators from '../components/donators'
-import favicon from '../public/favicon.ico'
+const faviconPath = process.env.NODE_ENV === 'development' ? '/favicon.ico' : '/next-monthly-digest/favicon.ico'
 function MyApp({ Component, pageProps }) {
   return <div className="App max-w-3xl mx-auto pb-6">
     <Head>
       <title>文章收藏 - next</title>
-      <link rel="icon" href={favicon} />
+      <link rel="icon" href={faviconPath} />
     </Head>
     <PageHeader/>
     <Component {...pageProps} />
