b4428490ea3224834979ae92fceb1f51e7a118bb
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-03-04 10:42:32 +0800

    增加标题、favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import "tailwindcss/tailwind.css"
+import Head from 'next/head'
 import PageHeader from '../components/page-header'
 import Donators from '../components/donators'
 function MyApp({ Component, pageProps }) {
   return <div className="App max-w-3xl mx-auto pb-6">
+    <Head>
+      <title>文章收藏 - next</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <PageHeader/>
     <Component {...pageProps} />
     <Donators/>
