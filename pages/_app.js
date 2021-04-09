af843c17477d7637dad9fd5ff8269f4288ff21c0
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-04-09 16:04:44 +0800

    删除`捐赠者`

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import "tailwindcss/tailwind.css"
 import Head from 'next/head'
 import PageHeader from '../components/page-header'
-import Donators from '../components/donators'
 function MyApp({ Component, pageProps }) {
   return <div className="App max-w-3xl mx-auto pb-6">
     <Head>
@@ -11,7 +10,6 @@ function MyApp({ Component, pageProps }) {
     </Head>
     <PageHeader/>
     <Component {...pageProps} />
-    <Donators/>
   </div>
 }
 
