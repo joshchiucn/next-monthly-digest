e1271d9b884d083047386abe6afc0ffedbe2c22e
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-04-12 16:18:55 +0800

    js => tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,8 @@ import "tailwindcss/tailwind.css"
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import PageHeader from '../components/page-header'
-function MyApp({ Component, pageProps } : AppProps) {
+
+function MyApp({ Component, pageProps }:AppProps) {
   return <div className="App max-w-3xl mx-auto pb-6">
     <Head>
       <title>文章收藏 - next</title>
