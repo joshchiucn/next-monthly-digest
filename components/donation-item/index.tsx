abd20e9fc070a789bbfc62aaed526c3e66693471
zhaoyouxin <youxin.zhao@visitshanghai.net>
2021-04-12 16:26:07 +0800

    remove unused files

--- components/donation-item/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FunctionComponent } from 'react'
-const DonationItem: FunctionComponent = () => (
-  <div className="donation-item"></div>
-)
-export default DonationItem
--- components/time/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from 'react'
-interface TimeProps {
-  time: string
-}
-const Time:FunctionComponent<TimeProps> = (props) => {
-  return <div className="time text-left text-gray-500 italic ">{props.time}</div>
-}
-export default Time
